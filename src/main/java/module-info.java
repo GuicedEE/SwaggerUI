b136d6c956998534e2fca74e57d93643540539f3
GedMarc <marc.magon@gmail.com>
2024-10-28 02:15:20 +0200

    Guiced Rabbit Configure Connections and Exchanges by Package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ import com.guicedee.servlets.swaggerui.SwaggerUIRegistration;
 
 open module com.guicedee.guicedservlets.swaggerui {
     requires guiced.vertx;
-    requires io.vertx;
+    requires transitive io.vertx.core;
 
     provides com.guicedee.vertx.spi.VertxRouterConfigurator with SwaggerUIRegistration;
 
